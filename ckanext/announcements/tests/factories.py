a470990c6148607890eb343533402a5ff5d6a3aa
Andres Vazquez <andres@data99.com.ar>
2022-07-08 11:21:22 -0300

    First test working

--- ckanext/announcements/tests/factories.py
+++ ckanext/announcements/tests/factories.py
@@ -10,7 +10,7 @@ class Announcement(factory.Factory):
         model = Announcement
 
     _user = helpers.call_action("get_site_user")
-    user_creator_id = _user['id']
+    user_creator_id = _user['name']
     from_date = datetime.datetime.utcnow() + datetime.timedelta(days=3)
     to_date = datetime.datetime.utcnow() + datetime.timedelta(days=7)
     message = 'This is an announcement message'
--- ckanext/announcements/tests/test_plugin.py
+++ ckanext/announcements/tests/test_plugin.py
@@ -4,7 +4,10 @@ from ckanext.announcements.tests import factories
 class TestAnnouncements:
 
     def setup(self):
-        self.announcement = factories.Announcement()
+        self.announcement = factories.Announcement(
+            message='This is an announcement message'
+        )
 
-    def test_plugin():
-        pass
+    def test_announcement_saved(self):
+        assert self.announcement.message == 'This is an announcement message'
+        assert self.announcement.status == 'active'
