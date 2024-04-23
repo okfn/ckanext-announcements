717c93dc7ae17f28ca8c35a1b10597cc0912e726
avdata99 <andres@data99.com.ar>
2024-04-23 12:51:03 -0300

    Properly display dates with TZ

--- ckanext/announcements/helpers.py
+++ ckanext/announcements/helpers.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+from pytz import timezone
 from ckan import model
 from ckan.plugins import toolkit
 from ckanext.announcements.models import Announcement
@@ -11,7 +12,7 @@ log = logging.getLogger(__name__)
 def get_public_announcements():
     """Get a list of Announcements"""
     # display messages up to 3 days after they disappeared.
-    now = datetime.datetime.utcnow()
+    now = datetime.datetime.now()
     messages = (
         model.Session.query(Announcement)
         .filter_by(status="active")
@@ -19,7 +20,7 @@ def get_public_announcements():
         .limit(10)
         .all()
     )
-
+    messages = _apply_tz(messages)
     return messages
 
 
@@ -33,5 +34,15 @@ def get_all_announcements():
         .limit(limit)
         .all()
     )
+    messages = _apply_tz(messages)
+    return messages
 
+
+def _apply_tz(messages):
+    """Apply the timezone to the messages"""
+    display_timezone = toolkit.config.get("ckan.display_timezone")
+    tz = timezone(display_timezone)
+    for message in messages:
+        message.from_date = message.from_date.astimezone(tz)
+        message.to_date = message.to_date.astimezone(tz)
     return messages
