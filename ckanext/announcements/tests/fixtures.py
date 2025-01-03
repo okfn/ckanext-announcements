2705304f26f97a441a3e07224a745b5b29316b32
avdata99 <andres@data99.com.ar>
2025-01-03 15:23:55 -0300

    Try to fix test

--- ckanext/announcements/tests/fixtures.py
+++ ckanext/announcements/tests/fixtures.py
@@ -1,27 +1,35 @@
 import contextlib
 import pytest
 
+from ckan.plugins import toolkit
 from ckan.cli.db import _resolve_alembic_config
 import ckan.model as model
 
 
-# TODO: in the next CKAN release
-# there will be a more elegant way to achieve this
-@contextlib.contextmanager
-def _repo_for_plugin(plugin):
-    original = model.repo._alembic_ini
-    model.repo._alembic_ini = _resolve_alembic_config(plugin)
-    try:
-        yield model.repo
-    finally:
-        model.repo._alembic_ini = original
+if toolkit.check_ckan_version(max_version='2.10'):
 
+    # TODO: in the next CKAN release
+    # there will be a more elegant way to achieve this
+    @contextlib.contextmanager
+    def _repo_for_plugin(plugin):
+        original = model.repo._alembic_ini
+        model.repo._alembic_ini = _resolve_alembic_config(plugin)
+        try:
+            yield model.repo
+        finally:
+            model.repo._alembic_ini = original
 
-def _apply_alembic_migrations():
-    with _repo_for_plugin("announcements") as repo:
-        repo.upgrade_db("head")
+    def _apply_alembic_migrations():
+        with _repo_for_plugin("announcements") as repo:
+            repo.upgrade_db("head")
 
+    @pytest.fixture
+    def announcement_migrate():
 
-@pytest.fixture
-def announcement_migrate():
-    _apply_alembic_migrations()
+        _apply_alembic_migrations()
+
+else:
+
+    @pytest.fixture
+    def announcement_migrate(migrate_db_for):
+        migrate_db_for('announcements')
